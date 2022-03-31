5d7179b85b35b7c153588c9a717979206b3ebb82
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-03-31 15:03:40 -0300

    Add repository dependencies to Telefone controller

--- src/main/java/br/com/zup/edu/fornecedormanager/controller/TelefoneController.java
+++ src/main/java/br/com/zup/edu/fornecedormanager/controller/TelefoneController.java
@@ -3,10 +3,22 @@ package br.com.zup.edu.fornecedormanager.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zup.edu.fornecedormanager.repository.FornecedorRepository;
+import br.com.zup.edu.fornecedormanager.repository.TelefoneRepository;
+
 @RestController
 @RequestMapping(FornecedorController.BASE_URI + "/{fornecedorId}" + TelefoneController.BASE_URI)
 public class TelefoneController {
 
     public final static String BASE_URI = "/telefones";
+
+    private final FornecedorRepository fornecedorRepository;
+    private final TelefoneRepository telefoneRepository;
+
+    public TelefoneController(FornecedorRepository fornecedorRepository,
+                              TelefoneRepository telefoneRepository) {
+        this.fornecedorRepository = fornecedorRepository;
+        this.telefoneRepository = telefoneRepository;
+    }
 
 }
